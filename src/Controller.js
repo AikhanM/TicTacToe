db7a0ae7441efc9b41b72118b867dce84e31419c
AikhanM <aikhanmirzaevi@gmail.com>
2023-08-09 13:59:34 +0400

    TicTacToe

---
+++ src/Controller.js
@@ -0,0 +1,71 @@
+export default class Controller {
+  constructor(Model, View) {
+    this.model = Model;
+    this.view = View;
+
+    this.view.cells.forEach((cell) => {
+      cell.addEventListener("click", () => {
+        this.play(cell.dataset.value);
+      });
+    });
+
+    this.view.newGameBtn.addEventListener("click", () => this.startNewGame());
+    this.view.resetBtn.addEventListener("click", () =>
+      this.resetClickHandler()
+    );
+
+    this.view.cells.forEach((cell, i) => {
+      cell.addEventListener("click", () => this.addMove(i));
+    });
+  }
+
+  play(i) {
+    this.model.play(i);
+    this.view.addToBoard(this.model.gameBoard);
+
+    if (this.model.winner) {
+      this.view.showModal(this.model.winner);
+      this.view.manageScore(this.model.playerXScore, this.model.playerOScore);
+    } else if (this.model.isBoardFull()) {
+      this.view.showModal("Draw");
+    }
+  }
+
+  addMove(i) {
+    if (
+      this.model.gameBoard[i] === "" &&
+      !this.model.winner
+    ) {
+      this.model.play(i);
+      const cell = this.cells[i];
+
+      if (this.model.winner) {
+        this.showModal(this.model.winner);
+        this.manageScore(
+          this.model.playerXScore,
+          this.model.playerOScore
+        );
+      } else if (this.model.isBoardFull()) {
+        this.showModal("Draw");
+      } else {
+        const clone = document.createElement("div");
+        clone.classList.add(this.model.curPlayer);
+        cell.appendChild(clone);
+      }
+    }
+  }
+
+  startNewGame() {
+    this.model.resetGame();
+    this.view.cells.forEach((cell) => {
+      cell.textContent = "";
+      cell.classList.remove("cross", "circle");
+    });
+    this.view.hideModal();
+  }
+
+  resetClickHandler() {
+    this.startNewGame();
+    this.view.manageScore(0, 0);
+  }
+}
---
+++ src/View.js
@@ -0,0 +1,47 @@
+export default class View {
+  constructor() {
+    this.cells = document.querySelectorAll(".cell");
+    this.newGameBtn = document.querySelector(".new");
+    this.resetBtn = document.querySelector(".reset");
+    this.modal = document.querySelector(".modal");
+    this.overlay = document.querySelector(".overlay");
+    this.message = document.querySelector(".message");
+
+    this.newGameBtn.addEventListener("click", () => this.startNewGame());
+    this.resetBtn.addEventListener("click", () => this.resetClickHandler());
+  }
+
+  addToBoard(board) {
+    this.cells.forEach((cell, i) => {
+      cell.textContent = board[i];
+    });
+  }
+
+  switchPlayers(curPlayer, cell) {
+    cell.classList.remove("circle");
+    cell.classList.remove("cross");
+    curPlayer === "cross"
+      ? cell.classList.add("cross")
+      : cell.classList.add("circle");
+  }
+
+  showModal(winner) {
+    this.modal.classList.remove("hidden");
+    this.overlay.classList.remove("hidden");
+    if (winner == "cross" || winner == "circle") {
+      this.message.innerHTML = `${winner.toUpperCase()} WINS!`;
+    } else if (winner === "Draw") {
+      this.message.textContent = "It's a Draw!";
+    }
+  }
+
+  hideModal() {
+    this.modal.classList.add("hidden");
+    this.overlay.classList.add("hidden");
+  }
+
+  manageScore(xScore, oScore) {
+    document.getElementById("playerXScore").textContent = xScore;
+    document.getElementById("playerOScore").textContent = oScore;
+  }
+}
