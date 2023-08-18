46f8ebe471d8fd2f7a553cf2e695dc5662b9d1f7
AikhanM <aikhanmirzaevi@gmail.com>
2023-08-19 01:10:55 +0400

    Added Webpack

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import Model from "./Model.js";
 import View from "./View.js";
 import Controller from "./Controller.js";
+import "../css/style.scss"
 
 window.addEventListener("DOMContentLoaded",function(){
     const model=new Model()
@@ -8,5 +9,4 @@ window.addEventListener("DOMContentLoaded",function(){
     const controller=new Controller(model,view)
     controller.init()
 })
-
 window.addEventListener("DOMContentLoaded",initialize)
